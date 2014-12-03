b5f10da735ef9e8c6bacdc29876aab9a6fe2f7b0
Jason Zhekov <jasssonpet@gmail.com>
2014-12-03 18:19:44 +0200

    Add a simple class to test fixtures.

---
+++ Source/NativeScript/NativeScript.Tests.Fixtures/Classes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NativeScript.Tests.Fixtures
+{
+    public sealed class SimpleClass
+    {
+        private const int InstanceReadonlyPropertyBackingField = 0;
+        public int InstanceReadonlyProperty
+        {
+            get { return InstanceReadonlyPropertyBackingField; }
+        }
+
+        private const int StaticReadonlyPropertyBackingField = 0;
+        public static int StaticReadonlyProperty
+        {
+            get { return StaticReadonlyPropertyBackingField; }
+        }
+
+        public int InstanceMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int StaticMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        private SimpleClass()
+        {
+        }
+    }
+}
