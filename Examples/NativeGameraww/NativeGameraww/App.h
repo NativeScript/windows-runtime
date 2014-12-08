adc0ddb41025d3804ed68ff6967307fcc07b88b5
Jason Zhekov <jasssonpet@gmail.com>
2014-12-09 01:50:02 +0200

    Add empty native Gameraww application.

---
+++ Examples/NativeGameraww/NativeGameraww/App.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <wrl.h>
+#include <windows.ui.xaml.h>
+
+namespace NativeGameraww {
+
+class App : public Microsoft::WRL::RuntimeClass<ABI::Windows::UI::Xaml::IApplicationOverrides> {
+    InspectableClass(L"NativeGameraww.App", BaseTrust);
+
+public:
+    STDMETHOD(OnActivated)(ABI::Windows::ApplicationModel::Activation::IActivatedEventArgs*) override;
+
+    STDMETHOD(OnLaunched)(ABI::Windows::ApplicationModel::Activation::ILaunchActivatedEventArgs*) override;
+
+    STDMETHOD(OnFileActivated)(ABI::Windows::ApplicationModel::Activation::IFileActivatedEventArgs*) override;
+
+    STDMETHOD(OnSearchActivated)(ABI::Windows::ApplicationModel::Activation::ISearchActivatedEventArgs*) override;
+
+    STDMETHOD(OnShareTargetActivated)(ABI::Windows::ApplicationModel::Activation::IShareTargetActivatedEventArgs*) override;
+
+    STDMETHOD(OnFileOpenPickerActivated)(ABI::Windows::ApplicationModel::Activation::IFileOpenPickerActivatedEventArgs*) override;
+
+    STDMETHOD(OnFileSavePickerActivated)(ABI::Windows::ApplicationModel::Activation::IFileSavePickerActivatedEventArgs*) override;
+
+    STDMETHOD(OnCachedFileUpdaterActivated)(ABI::Windows::ApplicationModel::Activation::ICachedFileUpdaterActivatedEventArgs*) override;
+
+    STDMETHOD(OnWindowCreated)(ABI::Windows::UI::Xaml::IWindowCreatedEventArgs*) override;
+
+    void App::SetBase(IApplicationOverrides* base) {
+        _base = base;
+    }
+
+private:
+    Microsoft::WRL::ComPtr<IApplicationOverrides> _base;
+};
+
+}
---
+++ Examples/NativeGameraww/NativeGameraww/pch.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <wrl.h>
+#include <windows.ui.xaml.h>
+
+#ifdef _DEBUG
+#define ASSERT_SUCCESS(hr)                                                     \
+    do {                                                                       \
+        HRESULT __hresult{hr};                                                 \
+        if (FAILED(__hresult)) {                                               \
+            ::RaiseException(__hresult, EXCEPTION_NONCONTINUABLE, 0, nullptr); \
+        }                                                                      \
+    } while (0)
+#else
+#define ASSERT_SUCCESS(hr) ((void)hr)
+#endif
