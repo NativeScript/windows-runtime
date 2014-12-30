e13f4f4c29dcb17c9ad6fd0317aa8ddbc04483bd
Jason Zhekov <jasssonpet@gmail.com>
2014-12-30 10:21:16 +0200

    Add more test fixtures for interface implementation.

--- Source/NativeScript/NativeScript.Tests.Fixtures/Interfaces.cs
+++ Source/NativeScript/NativeScript.Tests.Fixtures/Interfaces.cs
@@ -1,6 +1,77 @@
-﻿namespace NativeScript.Tests.Fixtures
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NativeScript.Tests.Fixtures
 {
     public interface IEmptyInterface
     {
+    }
+
+    public interface IExplicitInterface1
+    {
+        void Method();
+    }
+
+    public interface IExplicitInterface2
+    {
+        void Method();
+    }
+
+    public sealed class ExplicitInterfaceMethodImplementationClass : IExplicitInterface1, IExplicitInterface2
+    {
+        private ExplicitInterfaceMethodImplementationClass()
+        {
+        }
+
+        void IExplicitInterface1.Method()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IExplicitInterface2.Method()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Method()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class SingleGenericImplementationClass : IEnumerable<Int32>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class DoubleGenericImplementationClass : IEnumerable<Int16>, IEnumerable<Int32>
+    {
+        private DoubleGenericImplementationClass()
+        {
+        }
+
+        IEnumerator<Int32> IEnumerable<Int32>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<Int16> IEnumerable<Int16>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
