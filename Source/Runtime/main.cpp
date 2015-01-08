86beb8d97adc75cbdff8a55f0e147274ab522447
Jason Zhekov <jasssonpet@gmail.com>
2015-01-08 17:51:21 +0200

    Split the WindowsPhone and WindowsStore assets.

--- Source/Runtime/main.cpp
+++ Source/Runtime/main.cpp
@@ -3,15 +3,18 @@
 
 int main(int argc, char** argv) {
     JSGlobalContextRef ctx = JSGlobalContextCreate(nullptr);
-    
+
     return 0;
 }
 
+#define DLL_EXPORT extern "C" __declspec(dllexport)
+
+DLL_EXPORT
 BOOL WINAPI DllMain(
-	_In_  HINSTANCE hinstDLL,
-	_In_  DWORD fdwReason,
-	_In_  LPVOID lpvReserved
-	) {
-	main(0, nullptr);
-	return true;
+    _In_  HINSTANCE hinstDLL,
+    _In_  DWORD fdwReason,
+    _In_  LPVOID lpvReserved
+    ) {
+    main(0, nullptr);
+    return true;
 }
