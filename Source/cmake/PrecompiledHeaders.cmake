b88b9263d08b641bd0b352e18ecd0fbe147cc306
Jason Zhekov <jasssonpet@gmail.com>
2015-01-22 13:47:37 +0200

    Add a CMake command that groups files by folder.

--- Source/cmake/PrecompiledHeaders.cmake
+++ Source/cmake/PrecompiledHeaders.cmake
@@ -1,4 +1,4 @@
-macro(SET_PRECOMPILED_HEADER _target _header _source)    
+macro(SET_PRECOMPILED_HEADER _target _header _source)
     if (MSVC)
         set_target_properties(${_target} PROPERTIES COMPILE_FLAGS "/Yu${_header}")
         set_source_files_properties(${_source} PROPERTIES COMPILE_FLAGS "/Yc${_header}")
